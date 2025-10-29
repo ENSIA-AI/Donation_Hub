f379332660fba2cb3e4889b6d89f665e36b73a39
salsabilghennam <salsabil.ghennam@ensia.edu.dz>
2025-10-29 14:45:51 +0100

    made the buttinse login and register work -add transition-

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("#login") ;
+const btnR = document.querySelector("#register")
+const toggle = document.querySelector(".toggle-box");
+const toggleRight =  document.querySelector(".toggle-right")
+const toggleLeft = document.querySelector(".toggle-left")
+ btn.addEventListener('click' ,()=>{
+    toggle.classList.remove("toggle-box")
+    toggle.classList.add("toggle-box-active")
+    toggleRight.classList.remove("toggle-right");
+    toggleLeft.classList.add("toggle-left-active")
+})
+ btnR.addEventListener('click' ,()=>{
+    toggle.classList.remove("toggle-box-active")
+    toggle.classList.add("toggle-box")
+    toggleRight.style.display = "none";
+    toggleLeft.style.display = "block";
+})
+
