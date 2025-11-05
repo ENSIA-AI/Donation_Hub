e26bed5a4e636e7cce9c5db93a69c2cd31418c49
maramlahmar <maram.lahmar@ensia.edu.dz>
2025-11-05 15:19:32 +0100

    creating a file og_profile_script.js

---
+++ assets/JS/org_profile_script.js
@@ -0,0 +1,5 @@
+const see_more_org = document.querySelector('.see_more_btn');
+console.log(see_more_org);
+see_more_org.addEventListener('click', function () {
+    console.log("see more organizations  ");
+})
