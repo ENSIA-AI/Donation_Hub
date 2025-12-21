2c62286767d2b8ce89453e2f7d49f9005165a9a3
Hadj Messaoud Hayat <hayat.hadjmessaoud@ensia.edu.dz>
2025-12-22 00:39:40 +0100

    created all the models

---
+++ Backend/database/migrations/2025_12_21_233219_create_organizations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('org_name');
+            $table->date('org_registrationDate');
+            $table->text('org_description');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('wilaya_id')->constrained('wilayas');
+            $table->string('org_email');
+            $table->string('org_proof')->nullable(); 
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending'); 
+            
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('organizations');
+    }
+};
