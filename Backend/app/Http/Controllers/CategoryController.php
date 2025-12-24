0ed47084fa436237b7ca75ea6c42f2aa98541f98
maramlahmar <maram.lahmar@ensia.edu.dz>
2025-12-24 20:43:04 +0100

    solving the merge conflicts in main

--- Backend/app/Http/Controllers/CategoryController.php
+++ Backend/app/Http/Controllers/CategoryController.php
@@ -11,21 +11,14 @@ class CategoryController extends Controller
     // Get all categories
     public function index()
     {
-<<<<<<< HEAD
+
         return response()->json(Category::all(), 200);
 
-=======
-        return response()->json(Category::all());
->>>>>>> parent of a8748d6f (Merge branch 'CRUD-organization-regester')
     }
 
     // Create a new category
     public function store(Request $request)
     {
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of a8748d6f (Merge branch 'CRUD-organization-regester')
         $request->validate([
             'name' => 'required|string|max:255'
         ]);
