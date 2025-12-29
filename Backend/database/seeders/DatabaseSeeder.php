bd734fbaa9a9c097c613c6950c78f7b81a7819fb
maramlahmar <maram.lahmar@ensia.edu.dz>
2025-12-29 09:17:18 +0100

    changing the css file to add the search css

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
         OrganizationSeeder::class,
+        WilayaSeeder::class,
+        CategorySeeder::class,
          ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 } 
