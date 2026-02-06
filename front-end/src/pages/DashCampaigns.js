f2b1ff3efc5778d6970d6a62dd16d361e3cc3877
Hadj Messaoud Hayat <hayat.hadjmessaoud@ensia.edu.dz>
2026-02-06 08:20:18 +0100

    fixed the css of some components

--- front-end/src/pages/DashCampaigns.js
+++ front-end/src/pages/DashCampaigns.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import "../styles/dashCampaigns.css";
 import "../styles/AdminDashStat.css";
-
-import SearchBar from "../components/SearchBar";
 import CampaignRow from "../components/CampaignRow";
 import CampaignsTable from "../components/CampaignsTable";
 import Sidebar from "../components/Sidebar";
@@ -17,6 +15,31 @@ const DashCampaigns = () => {
       .then((data) => setCampaigns(data))
       .catch((err) => console.error(err));
   };
+
+  const handleRejectCampaign = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:8000/api/compaigns/${id}/reject`, {
+      method: 'PATCH', // or PATCH depending on your route
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert(data.message); // optional
+      // Remove the rejected campaign from the state
+      setCampaigns(campaigns.filter(c => c.compaign_ID !== id));
+    } else {
+      alert(data.message || 'Error rejecting campaign');
+    }
+  } catch (err) {
+    console.error(err);
+    alert('Network error while rejecting campaign');
+  }
+};
+
 
   useEffect(() => {
     fetchCampaigns();
@@ -44,20 +67,16 @@ const DashCampaigns = () => {
 
               {campaigns.map((campaign) => (
               <CampaignRow
-               key={campaign.compaign_ID}
-               title={campaign.compaign_title}
-               date={new Date(campaign.compaign_date).toLocaleDateString()}
-               organization={campaign.organization?.org_name}
-               wilaya={campaign.organization?.wilaya?.name}
-              />
-           ))}
-
-
-              <div className="see-more-container">
-                <div className="see-more-button">
-                  <i className="fas fa-plus"></i>
-                </div>
-              </div>
+                     key={campaign.compaign_ID}
+                     compaignId={campaign.compaign_ID}
+                     title={campaign.compaign_title}
+                     date={new Date(campaign.compaign_date).toLocaleDateString()}
+                     organization={campaign.organization?.org_name}
+                     wilaya={campaign.organization?.wilaya?.wilaya_name}
+                     category={campaign.organization?.category?.category}
+                     onReject={handleRejectCampaign} // <--- pass handler
+       />
+        ))}    
 
             </div>
           </div>
